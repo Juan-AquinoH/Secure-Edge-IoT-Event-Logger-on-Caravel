274d89c950c2933f5bc396091ff581dfd210b806
Baavanes <baavanesh.s@kloksys.com>
2026-03-13 15:55:47 +0530

    Update secure_top.v

--- verilog/rtl/secure_top.v
+++ verilog/rtl/secure_top.v
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a3be16d413e7d92208f18b01a960876e12556da0817036560b37917cbb0f2df
-size 2180
+oid sha256:3b7312d5149222c6bb7da98a6769f9f23e54dd44923ec153bd46da88cadf91cd
+size 2348
