fd6939cdea6c31bdb8f09d0f6c8f42f8a4467967
Juan-AquinoH <juancarlos.aquino@utnay.edu.mx>
2026-03-21 07:27:40 -0600

    Clean repo: remove OpenLane, sim and physical artifacts

--- sim/hello_world/RTL-compilation/includes.v
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1ac16e8c93f3391e3e85ea83a2dc90a560adb204b8d3c6499bf230f755370d7c
-size 5866
--- sim/run_26_Nov_17_45_48_74/RTL-secure_test1_asserts/module_trail.py
+++
@@ -1,4 +0,0 @@
-from os import path
-import sys
-sys.path.append(path.abspath('/home/carlos/caravel_secure/caravel_user_neuromorphic_secure/verilog/dv/cocotb'))
-from cocotb_tests import *
